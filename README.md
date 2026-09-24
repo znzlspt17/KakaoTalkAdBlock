642ecc7eab311973648f82e7a7fa2e443f020cb8
znzlspt17 <znzlspt17@gmail.com>
2026-09-24 12:54:23 +0900

    feat: systray 기능 삭제 - 종료시에는 반드시 작업관리자를 통해 프로세스를 종료해야함

--- README.md
+++ README.md
@@ -6,10 +6,5 @@ AdBlocker for KakaoTalk Windows client.
 
 ![](https://raw.githubusercontent.com/blurfx/KakaoTalkAdBlock/main/kakaotalk.png)
 
-This program runs in the background.
-
-Right-click the tray icon to quit, or to check for new versions.
-
-*Earlier versions of 2.2.0 can be quit by double-clicking the tray icon.*
-
-![](https://raw.githubusercontent.com/blurfx/KakaoTalkAdBlock/main/tray.png)
+This program runs in the background without a tray icon or visible window.
+To quit, end `KakaoTalkAdBlock` in Task Manager. To disable an existing run-on-startup setting, use Windows Startup Apps settings.
--- cmd/main.go
+++ cmd/main.go
@@ -5,65 +5,11 @@ package main
 import (
 	"context"
 	"kakaotalkadblock/internal"
-	"kakaotalkadblock/internal/win"
-	"kakaotalkadblock/winres"
-	"os/exec"
-
-	"github.com/energye/systray"
 
 	_ "kakaotalkadblock/winres"
 )
 
-const VERSION = "2.2.3"
-
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	destroy := func() {
-		cancel()
-		systray.Quit()
-	}
-	systray.SetOnRClick(func(menu systray.IMenu) {
-		menu.ShowMenu()
-	})
-	systray.Run(func() {
-		systray.SetIcon(winres.IconData)
-		systray.SetTooltip("KakaoTalkAdBlock")
-		systray.AddMenuItem(VERSION, VERSION).Disable()
-		checkRelease := systray.AddMenuItem("", "Check latest releases")
-		checkRelease.Click(func() {
-			exec.Command(
-				"rundll32",
-				"url.dll,FileProtocolHandler",
-				"https://github.com/blurfx/KakaoTalkAdBlock/releases",
-			).Start()
-		})
-		checkRelease.Hide()
-		systray.AddSeparator()
-		go func() {
-			tagName, hasNewRelease := internal.CheckLatestVersion(VERSION)
-			if hasNewRelease {
-				checkRelease.SetTitle("New version available: " + tagName)
-				checkRelease.Show()
-			}
-		}()
-
-		startupItem := systray.AddMenuItem("Run on startup", "Run on startup")
-		if win.IsStartupEnabled() {
-			startupItem.Check()
-		}
-		startupItem.Click(func() {
-			if startupItem.Checked() {
-				startupItem.Uncheck()
-				win.SetStartupEnabled(false)
-			} else {
-				startupItem.Check()
-				win.SetStartupEnabled(true)
-			}
-		})
-		systray.AddMenuItem("E&xit", "Exit").Click(destroy)
-
-		internal.Run(ctx)
-	}, func() {
-		cancel()
-	})
+	internal.Run(context.Background())
+	select {}
 }
